53eed5afc8e5068f7591419e4c4a619e6621db7a
smsdahanayaka <smsdahanayaka@gmail.com>
2025-05-14 11:59:40 +0530

    off debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Motel Reservation',
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Poppins',
